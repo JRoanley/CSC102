94b1b7d1eaa79be003fc6ac9b37ed22befde9464
JRoanley <jroanley91138@uat.edu>
2025-04-19 17:40:11 -0400

    Initial strings.html and updated code.js

--- code.js
+++ code.js
@@ -33,5 +33,74 @@ function rollDice() {
     }
 }
 
+// Addition of a palindrome checker
+function checkForPalindrome() {
+
+    // Do palindrome check
+    let inputField = document.getElementById("palindromeField");
+    let results = document.getElementById("palindromeResults")
+    //console.log(inputField);
+
+    // example of palindrome
+    let inputValue = inputField.value;
+    let stringLength = inputValue.length;
+    if (stringLength > 0) {
+        
+        // math for the palindrome check
+        let iterationCount = Math.floor(stringLength/2);
+        let lastCharacterIndex = inputValue.length - 1;
+        let palindromeCheckResult = true;
+
+        // Allowed to continue the plaindrome check
+        for(let i = 0; i < iterationCount; i++) {
+        if (inputValue[i] !== inputValue[lastCharacterIndex - i]) {
+            console.log("NOT A PALINDROME!");
+            results.innerHTML = "NOT A PALINDROME!"
+            palindromeCheckResult = false;
+            break;
+          }
+        }
+        if (palindromeCheckResult) {
+            console.log("This is a palindrome!!")
+            results.innerHTML = "This is a palindrome!!"
+        }
+    }
+}
+
+// Addition of user varification
+function userInfoValidation() {
+
+    console.log("Checking User Info.");
+
+    let firstNameField = document.getElementById("firstNameField");
+    let lastNameField = document.getElementById("lastNameField");
+    let zipField = document.getElementById("zipCode");
+    let results = document.getElementById("userInfoResults");
+    results.innerHTML = "";
+    let firstNameValue = firstNameField.value;
+    let lastNameValue = lastNameField.value;
+    let zipValue = zipField.value;
+
+    // concatenation, verify name length
+    let firstLastName = firstNameValue + " " + lastNameValue;
+    console.log(firstLastName); 
+    
+    // verify name length
+    if (firstLastName.length > 20) {
+        results.innerHTML = "WAY TOO LONG! NEEDS TO BE UNDER 20!"
+        return; 
+    }
+
+    // verify zipcode value and checks that it is digit
+    let zipRegex = /^\d{5}$/
+    if (!zipRegex.test(zipValue)) {
+    results.innerHTML = "WHOA WRONG FORMAT! NEEDS TO BE 5 DIGITS!"
+    return;
+    }
+
+    let zipNumValue = parseInt(zipValue);
+    console.log(zipValue);
+    console.log("Input validated");
+}
     
 
